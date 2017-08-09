59526f282c2e93c936d91cbec06e5741a231a331
abler <mail@xiechaoyi.com>
2017-08-09 14:55:40 +0800

    Version 1.8.4

--- GitPush.bat
+++ GitPush.bat
@@ -1,10 +1,9 @@
 @echo off
-title GitPush工具 By Wlniao
-set /p input=请输入当前发布的版本号（如:v160101）：
-if "%input%"=="" (
-    echo Auto Push
+set /p tags=请输入当前发布的版本号（latest）：
+if "%tags%"=="" (
     git add -A && git commit -a -m "GitPush" && git push -u origin master
+    git tag -f latest -m "By GitPush." && git push -u origin master latest -f
 ) else (
-    git add -A && git commit -a -m "Version %input%" && git push -u origin master
-    git tag -a %input% -m "By GitPush." && git push --tags -u origin master
+    git add -A && git commit -a -m "Version %tags%" && git push -u origin master
+    git tag -f %tags% -m "By GitPush." && git push -u origin master %tags% -f
 )
